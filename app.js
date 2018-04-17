0537547ac07a85dc0dd972e1dce94917ee03f919
Dirk Baeumer <dirkb@microsoft.com>
2018-04-17 11:46:46 +0200

    Add a second server

--- app.js
+++ app.js
@@ -5,22 +5,41 @@
 'use strict';
 exports.__esModule = true;
 var net = require("net");
-var server = net.createServer(function (connection) {
+var server7999 = net.createServer(function (connection) {
+    console.log("Server received a connection on 7999");
+    connection.on('data', function (data) {
+        console.log('Server 7999 Received data');
+        console.log(data.toString('utf8'));
+    });
+    connection.on('close', function () {
+        console.log('7999 connection got closed.');
+    });
+});
+server7999.on('error', function (err) {
+    console.error('Server 7999 received error');
+    if (err) {
+        console.error(err);
+    }
+});
+server7999.listen(7999, function () {
+    console.log('Server listening on 7999...');
+});
+var server8000 = net.createServer(function (connection) {
     console.log("Server received a connection on 8000");
     connection.on('data', function (data) {
-        console.log('Received data');
+        console.log('Server 8000 received data');
         console.log(data.toString('utf8'));
     });
     connection.on('close', function () {
         console.log('8000 connection got closed.');
     });
 });
-server.on('error', function (err) {
-    console.error('Server received error');
+server8000.on('error', function (err) {
+    console.error('Server 8000 received error');
     if (err) {
         console.error(err);
     }
 });
-server.listen(8000, function () {
+server8000.listen(8000, function () {
     console.log('Server listening on 8000...');
 });
--- app.ts
+++ app.ts
@@ -6,10 +6,32 @@
 
 import * as net from 'net';
 
-const server = net.createServer((connection) => {
+const server7999 = net.createServer((connection) => {
+	console.log(`Server received a connection on 7999`);
+	connection.on('data', (data) => {
+		console.log('Server 7999 Received data');
+		console.log(data.toString('utf8'));
+	});
+	connection.on('close', () => {
+		console.log('7999 connection got closed.');
+	});
+});
+
+server7999.on('error', (err) => {
+	console.error('Server 7999 received error');
+	if (err) {
+		console.error(err);
+	}
+});
+
+server7999.listen(7999, () => {
+	console.log('Server listening on 7999...');
+});
+
+const server8000 = net.createServer((connection) => {
 	console.log(`Server received a connection on 8000`);
 	connection.on('data', (data) => {
-		console.log('Received data');
+		console.log('Server 8000 received data');
 		console.log(data.toString('utf8'));
 	});
 	connection.on('close', () => {
@@ -17,13 +39,13 @@ const server = net.createServer((connection) => {
 	});
 });
 
-server.on('error', (err) => {
-	console.error('Server received error');
+server8000.on('error', (err) => {
+	console.error('Server 8000 received error');
 	if (err) {
 		console.error(err);
 	}
 });
 
-server.listen(8000, () => {
+server8000.listen(8000, () => {
 	console.log('Server listening on 8000...');
 });
